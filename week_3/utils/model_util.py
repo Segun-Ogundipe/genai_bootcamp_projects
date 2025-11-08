d4a3ed2940254c211de6b9e8cd89517230569536
Segun David Ogundipe <segun.d.ogundipe@gmail.com>
2025-11-08 22:29:33 +0100

    Add supported llm and embedding models

---
+++ week_3/utils/model_util.py
@@ -0,0 +1,24 @@
+SUPPORTED_GROQ_MODELS = [
+        "llama-3.1-8b-instant",
+        "llama-3.3-70b-versatile",
+        "openai/gpt-oss-120b",
+        "openai/gpt-oss-20b"
+]
+
+SUPPORTED_OPENAI_MODELS = [
+        "gpt-5-nano-2025-08-07",
+        "gpt-5-mini-2025-08-07",
+        "gpt-5-2025-08-07",
+]
+
+SUPPORTED_OPENAI_EMBEDDING_MODELS = [
+        "text-embedding-3-small",
+        "text-embedding-3-large",
+        "text-embedding-ada-002",
+]
+
+SUPPORTED_HUGGINGFACE_EMBEDDING_MODELS = [
+        "all-MiniLM-L6-v2",
+        "nomic-ai/nomic-embed-text-v1",
+        "google/embeddinggemma-300m",
+]
