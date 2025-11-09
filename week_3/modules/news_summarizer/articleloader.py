745339c96fcf029cb0cefd6b9a8eda10e824027e
Segun David Ogundipe <segun.d.ogundipe@gmail.com>
2025-11-09 16:09:59 +0100

    Organise import in articleloader.py

--- week_3/modules/news_summarizer/articleloader.py
+++ week_3/modules/news_summarizer/articleloader.py
@@ -2,7 +2,6 @@ from typing import List
 
 from langchain_community.document_loaders.base import BaseLoader
 from langchain_core.documents import Document
-
 from newspaper import Article
 
 class ArticleLoader(BaseLoader):
